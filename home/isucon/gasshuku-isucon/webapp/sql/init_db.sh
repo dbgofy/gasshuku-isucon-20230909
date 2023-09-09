45ad9424023f33567c0b63580622cad56499c57e
bgpat <bgpat@users.noreply.github.com>
2023-09-09 14:53:15 +0000

    Optimize mysql

--- home/isucon/gasshuku-isucon/webapp/sql/init_db.sh
+++ home/isucon/gasshuku-isucon/webapp/sql/init_db.sh
@@ -8,11 +8,10 @@ DB_PASS=${DB_PASS:-isucon}
 DB_NAME=${DB_NAME:-isulibrary}
 
 set -x
-
-cat 0_schema.sql 1_data.sql | mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME"
-
-service mysql restart --innodb-doublewrite=0
-mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 2_book_title_suffix.sql
-mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 2_book_author_suffix.sql
+mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 0_schema.sql
+mysql <<< 'ALTER INSTANCE DISABLE INNODB REDO_LOG; SET GLOBAL slow_query_log = 0;'
+mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 1_data.sql &
+mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 2_book_title_suffix.sql &
+mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 2_book_author_suffix.sql &
 wait
-systemctl restart mysql
+mysql <<< 'ALTER INSTANCE ENABLE INNODB REDO_LOG; SET GLOBAL slow_query_log = 1;'
