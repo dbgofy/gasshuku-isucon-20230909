c775e5ed0184b62030607b059238a2d2c12ecc3a
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2023-09-09 16:08:44 +0900

    NamedExecContext

--- home/isucon/gasshuku-isucon/webapp/go/main.go
+++ home/isucon/gasshuku-isucon/webapp/go/main.go
@@ -346,11 +346,11 @@ func initializeHandler(c echo.Context) error {
 		}
 	}
 	// bulk insert
-	_, err = db.ExecContext(c.Request().Context(), "INSERT INTO `book_title_suffix` (`book_id`, `title_suffix`) VALUES (?, ?)", bookTitleSuffixes)
+	_, err = db.NamedExecContext(c.Request().Context(), "INSERT INTO `book_title_suffix` (`book_id`, `title_suffix`) VALUES (:id , :suffix_title)", bookTitleSuffixes)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
-	_, err = db.ExecContext(c.Request().Context(), "INSERT INTO `book_author_suffix` (`book_id`, `author_suffix`) VALUES (?, ?)", bookAuthorSuffixes)
+	_, err = db.NamedExecContext(c.Request().Context(), "INSERT INTO `book_author_suffix` (`book_id`, `author_suffix`) VALUES (:id , :suffix_author)", bookAuthorSuffixes)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
