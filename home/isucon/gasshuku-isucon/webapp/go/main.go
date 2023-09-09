50d4a18feafab8b342e737eab81a5303b39b2ce9
bgpat <bgpat23@gmail.com>
2023-09-09 18:48:02 +0900

    Revert "Merge pull request #17 from dbgofy/shiage"
    This reverts commit 097d46bd541d6f6b55d624c61bb2eb437dedaec0, reversing
    changes made to 002d0b28b37ec4f40ab8a1bf038d9ab64f9ebdd3.

--- home/isucon/gasshuku-isucon/webapp/go/main.go
+++ home/isucon/gasshuku-isucon/webapp/go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
@@ -8,6 +9,10 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"github.com/uptrace/opentelemetry-go-extra/otelsql"
+	"github.com/uptrace/opentelemetry-go-extra/otelsqlx"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"io"
 	"log"
 	"net/http"
@@ -22,10 +27,17 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/oklog/ulid/v2"
+	"github.com/uptrace/opentelemetry-go-extra/otelplay"
 )
 
 func main() {
+	ctx := context.Background()
+
+	shutdown := otelplay.ConfigureOpentelemetry(ctx)
+	defer shutdown()
+
 	host := getEnvOrDefault("DB_HOST", "localhost")
 	port := getEnvOrDefault("DB_PORT", "3306")
 	user := getEnvOrDefault("DB_USER", "isucon")
@@ -34,7 +46,7 @@ func main() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Asia%%2FTokyo", user, pass, host, port, name)
 
 	var err error
-	db, err = sqlx.Open("mysql", dsn)
+	db, err = otelsqlx.Open("mysql", dsn, otelsql.WithAttributes(semconv.DBSystemMySQL))
 	if err != nil {
 		log.Panic(err)
 	}
@@ -52,6 +64,9 @@ func main() {
 	}
 
 	e := echo.New()
+	e.Debug = true
+	e.Use(middleware.Logger())
+	e.Use(otelecho.Middleware("dev-1"))
 
 	api := e.Group("/api")
 	{
