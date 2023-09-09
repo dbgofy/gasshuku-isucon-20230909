58b0c25d03b6284c6aa89a544a64ab8251cfcfb8
bgpat <bgpat@users.noreply.github.com>
2023-09-09 14:22:38 +0000

    Disable innodb doublewrite during importing

--- home/isucon/gasshuku-isucon/webapp/sql/init_db.sh
+++ home/isucon/gasshuku-isucon/webapp/sql/init_db.sh
@@ -7,4 +7,12 @@ DB_USER=${DB_USER:-isucon}
 DB_PASS=${DB_PASS:-isucon}
 DB_NAME=${DB_NAME:-isulibrary}
 
-cat 0_schema.sql 1_data.sql 2_*.sql | mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME"
+set -x
+
+cat 0_schema.sql 1_data.sql | mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME"
+
+service mysql restart --innodb-doublewrite=0
+mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 2_book_title_suffix.sql
+mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 2_book_author_suffix.sql
+wait
+systemctl restart mysql
