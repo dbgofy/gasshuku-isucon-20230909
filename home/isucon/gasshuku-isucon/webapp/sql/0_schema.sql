c1aae282781d7e3f2f78eafc355fc06fe28437ef
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2023-09-10 01:32:04 +0900

    COLLATEを指定

--- home/isucon/gasshuku-isucon/webapp/sql/0_schema.sql
+++ home/isucon/gasshuku-isucon/webapp/sql/0_schema.sql
@@ -48,11 +48,11 @@ DROP TABLE IF EXISTS `book_title_suffix`;
 CREATE TABLE `book_title_suffix` (
   `book_id` varchar(26) NOT NULL,
   `title_suffix` varchar(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE = InnoDB COLLATE = utf8mb4_bin DEFAULT CHARSET = utf8mb4;
 
 DROP TABLE IF EXISTS `book_author_suffix`;
 
 CREATE TABLE `book_author_suffix` (
    `book_id` varchar(26) NOT NULL,
    `author_suffix` varchar(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+) ENGINE = InnoDB COLLATE = utf8mb4_bin DEFAULT CHARSET = utf8mb4;
