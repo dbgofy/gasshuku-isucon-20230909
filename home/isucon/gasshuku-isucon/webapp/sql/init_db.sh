4efc1b4728ce41d12c20767a7b23b518fab602bb
Goryudyuma <6211370+Goryudyuma@users.noreply.github.com>
2023-09-12 19:26:30 +0900

    fix

--- home/isucon/gasshuku-isucon/webapp/sql/init_db.sh
+++ home/isucon/gasshuku-isucon/webapp/sql/init_db.sh
@@ -12,14 +12,14 @@ set -x
 mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 0_schema.sql
 mysql <<< 'ALTER INSTANCE DISABLE INNODB REDO_LOG; SET GLOBAL slow_query_log = 0;'
 
-mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 1_data.sql &
+mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 1_data.sql # &
 
 # mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 2_book_title_suffix.sql &
 # mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 2_book_author_suffix.sql &
 
-wait
+# wait
 
 # date
 
 # mysql -h"$DB_HOST" -P"$DB_PORT" -u"$DB_USER" -p"$DB_PASS" "$DB_NAME" < 3_index.sql
-# mysql <<< 'ALTER INSTANCE ENABLE INNODB REDO_LOG; SET GLOBAL slow_query_log = 1;'
+mysql <<< 'ALTER INSTANCE ENABLE INNODB REDO_LOG; SET GLOBAL slow_query_log = 1;'
